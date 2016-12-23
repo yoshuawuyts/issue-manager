64ca9d657877b14bf4c846eca8d7ad0dee8a10e8
lrlna <shestak.irina@gmail.com>
2016-12-24 00:35:15 +0100

    add township-auth

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+var basic = require('township-auth/basic')
+var Auth = require('township-auth')
 var bankai = require('bankai')
-// var Github = require('github')
 var merry = require('merry')
+var memdb = require('memdb')
 var path = require('path')
 
 var notFound = merry.notFound
 
 var clientPath = path.join(__dirname, 'client.js')
 var assets = bankai(clientPath)
-// var github = new Github()
 
 var env = merry.env({ PORT: 8080 })
 var app = merry()
+
+var db = memdb()
+
+var auth = Auth(db, {
+  providers: { basic: basic }
+})
+
+
+function createAuth (ctx) {
+}
+
+
+
+createAuth({email: 'none@none.ca', password: '1234' })
 
 app.router([
   [ '/', function (req, res, ctx, done) {
@@ -22,7 +37,45 @@ app.router([
   [ '/bundle.css', function (req, res, ctx, done) {
     done(null, assets.css(req, res))
   }],
+  [ '/register', function (req, res, ctx, done) {
+    auth.create({
+      basic: {
+        email: 'none@none.ca',
+        password: '1234'
+      }
+    }, function (err, account) {
+      if (err) return merry.error(400, 'cannot create a user account', err)
+      done(null, 'ok')
+    })
+  }],
+  [ '/logout', function (req, res, ctx, done) {
+    done(null, assets.js(req, res))
+  }],
+  [ '/verify', function (req, res, ctx, done) {
+    auth.verify('basic', {
+      email: 'none@none.ca',
+      password: '1234'
+    }, function (err, result) {
+      if (err) return merry.error(400, 'cannot verify user account', err)
+      done(null, 'verified ok')
+    })
+  }],
   [ '/404', notFound() ]
 ])
+
+
+
+// /login
+// get user information, send to github for verification
+// receive token back
+
+// /redirect
+// redirect url given to github to redirect to
+
+// /logout
+// removes token from loca storage
+
+// /register
+// /verify
 
 app.listen(env.PORT)
